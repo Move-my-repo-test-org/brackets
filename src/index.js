7954ce9fd365617cdfa36859bf114abc143d2676
NastiaKoval <anasteziyam@gmail.com>
2020-03-21 13:26:04 +0200

    feat: make a check func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2) {
+    return false;
+  }
+  
+  let arrOfStr = [];
+  bracketsConfig.forEach(arr => {
+    arrOfStr.push(arr.join(''));
+  });
+
+  for (let i = 0; i < arrOfStr.length; i++) {
+    if (str.includes(arrOfStr[i])) {
+      str = str.replace(arrOfStr[i], '');
+      i = -1;
+    }
+  }
+
+  if (str) {
+    return false;
+  } else {
+    return true;
+  }
+
 }
